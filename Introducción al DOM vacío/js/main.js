f0a5f52c72403692bed8b4df87cee3d48a984715
Mattia Lu <mlu481@g.educaand.es>
2023-10-02 13:31:12 +0200

    1

---
+++ Introducción al DOM vacío/js/main.js
@@ -0,0 +1,51 @@
+
+////Modificar DIV primera celda (id: f1c1)
+let celda1 = document.getElementById("f1c1");
+//contenido etiqueta
+alert(celda1.innerHTML)
+celda1.innerHTML="<b>hola</b>";
+alert(celda1.innerText)
+celda1.innerText="<b>Hola</b>";
+
+// asignar innerText que incluya etiquetas HTML
+
+
+////Modifivar DIV segunda celda (id: f1c2)
+
+// asignar innerHTML que incluya etiquetas HTML
+
+////Modificar DIV tercera celda (id: f1c3) asignando estilo CSS (color y fontsize)
+
+
+////Modificar imagen (https://e7.pngegg.com/pngimages/602/440/png-clipart-javascript-open-logo-number-js-angle-text.png)
+
+
+////Aumentar texto de la fila 2 (todos los div dentro de fila2) usando $
+
+
+
+////Cambiar texto de la última celda (id: f2c3), texto pedido al usuario.
+
+
+////Función para la pulsación del botón
+
+//boton_click: añadir * en título
+
+//boton_click2: añadir - en título
+
+//Asignación de función a la pulsación del botón (click, mousedown, mouseup)
+
+//Restauración del título al hacer clic en el mismo.
+
+
+////Añadir nuevos elementos HTML al final de un elemento existente (id: ultimaFila)
+//Crear una nueva fila div-row, con un elemento columna div-col (con bg-danger).
+
+//Añadir al final del contenedor (container || container-fluid)
+
+
+////Añadir nuevo elemento HTML junto a un elemento existente (antes de ultimaFila)
+////element.insertAdjacentElement(position, element); //beforbegin, afterbegin, beforeend, afterend
+//Crear una nueva fila div-row, con un elemento columna div-col (con bg-warning).
+
+//Añadir antes del div con id ultimaFila
