c6b07b6ad7cc5e39178a95b509798b4a4a47cd6b
Mattia Lu <mlu481@g.educaand.es>
2023-10-03 11:49:47 +0200

    1

--- Introducción al DOM vacío/js/main.js
+++ Introducción al DOM vacío/js/main.js
@@ -1,40 +1,52 @@
 
 ////Modificar DIV primera celda (id: f1c1)
-let celda1 = document.getElementById("f1c1");
-//contenido etiqueta
-alert(celda1.innerHTML)
-celda1.innerHTML="<b>hola</b>";
-alert(celda1.innerText)
-celda1.innerText="<b>Hola</b>";
+let celda = document.getElementById("f1c1");
+celda.innerText="Primera celda";
 
 // asignar innerText que incluya etiquetas HTML
-
+celda.innerText="<b>Primera celda</b>";
 
 ////Modifivar DIV segunda celda (id: f1c2)
-
+celda = document.getElementById("f1c2");
+celda.innerText="Segunda celda";
 // asignar innerHTML que incluya etiquetas HTML
-
+celda.innerHTML="<b>Segunda celda</b>";
 ////Modificar DIV tercera celda (id: f1c3) asignando estilo CSS (color y fontsize)
-
-
+celda = document.querySelector("#f1c3");
+celda.style.color="red";
+celda.style.fontSize="2em";
 ////Modificar imagen (https://e7.pngegg.com/pngimages/602/440/png-clipart-javascript-open-logo-number-js-angle-text.png)
-
-
+const img=document.querySelector("img");
+img.src="https://e7.pngegg.com/pngimages/602/440/png-clipart-javascript-open-logo-number-js-angle-text.png";
+img.width="200";
+img.alt="Logo de JavaScript"
 ////Aumentar texto de la fila 2 (todos los div dentro de fila2) usando $
-
+const fila2=document.querySelectorAll("div#fila2>div.col");
+fila2.forEach(c => {
+    c.innerHTML=`<h2>${c.innerHTML}</h2>`;
+});
 
 
 ////Cambiar texto de la última celda (id: f2c3), texto pedido al usuario.
-
+let txt=prompt("txt: ");
+celda=document.getElementById("f2c3");
+celda.innerText=`${txt}`;
 
 ////Función para la pulsación del botón
-
+const button=document.getElementById("boton");
+const title=document.querySelector("h1");
 //boton_click: añadir * en título
-
+function boton_click() {
+    title.innerText=title.innerHTML+"*";
+}
 //boton_click2: añadir - en título
-
+function boton_click2() {
+    
+    title.innerText=title.innerHTML+"-";
+}
 //Asignación de función a la pulsación del botón (click, mousedown, mouseup)
-
+button.click=boton_click();
+button.onmousedown=boton_click2();
 //Restauración del título al hacer clic en el mismo.
 
 
