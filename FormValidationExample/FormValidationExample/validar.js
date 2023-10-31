6d820d61d36c280c4074c7ab6165f12f28faeb7a
Mattia Lu <mlu481@g.educaand.es>
2023-10-31 09:47:19 +0100

    1

--- FormValidationExample/FormValidationExample/validar.js
+++ FormValidationExample/FormValidationExample/validar.js
@@ -4,49 +4,47 @@ const email=document.querySelector("#email");
 const edad=document.querySelector("#edad");
 const boton=document.querySelector("#submit");
 
+const nombreError=document.querySelector("#nombreError span");
+const emailError=document.querySelector("#emailError span");
+const edadError=document.querySelector("#edadError span");
 
-//PASO 1: Personalizar mensaje de validación
-//Indicar mayoría de edad
-function validaEdad() {
-    if (edad.validity.valueMissing){
-        edad.setCustomValidity("La edad es obligatoria");
-    }else if (edad.validity.rangeOverflow) {
-        edad.setCustomValidity("La edad es demasiada cantidad");
-    }else if (edad.validity.rangeUnderflow) {
-        edad.setCustomValidity("Debe ser mayor de edad");
-    }else{
-        edad.setCustomValidity("");
-    }
-}
-edad.addEventListener('input',validaEdad)
-//Paso 2: Configurar mensaje la primera vez
-validaEdad()
+// //PASO 1: Personalizar mensaje de validación
+// //Indicar mayoría de edad
+// function validaEdad() {
+//     if (edad.validity.valueMissing){
+//         edad.setCustomValidity("La edad es obligatoria");
+//     }else if (edad.validity.rangeOverflow) {
+//         edad.setCustomValidity("La edad es demasiada cantidad");
+//     }else if (edad.validity.rangeUnderflow) {
+//         edad.setCustomValidity("Debe ser mayor de edad");
+//     }else{
+//         edad.setCustomValidity("");
+//     }
+// }
+// edad.addEventListener('input',validaEdad)
+// //Paso 2: Configurar mensaje la primera vez
+// validaEdad()
 
 
-//PASO 3: Segunda validación tras la automática del navegador por HTML5
-// correo electrónico de dominio @gmail.com
+// //PASO 3: Segunda validación tras la automática del navegador por HTML5
+// // correo electrónico de dominio @gmail.com
 
 function extraeDominio() { //Se usa en paso 3 y paso 5.
     let correo=email.value
     return correo.substring(correo.indexOf("@")+1)
 }
 
-function validaEmail(){
-    if (email.validity.valueMissing){
-        email.setCustomValidity("El email es obligatorio");
-    }else if (extraeDominio()!="gmail.com") {
-        email.setCustomValidity("El dominio debe ser @gmail.com")
-    }else{
-        email.setCustomValidity("")
-    }
-}
-email.addEventListener('blur',validaEmail)
-validaEmail()
-function validaFormulario(event) {
-    validaEdad();
-    validaEmail();
-}
-boton.addEventListener('click',validaFormulario)
+// function validaEmail(){
+//     if (email.validity.valueMissing){
+//         email.setCustomValidity("El email es obligatorio");
+//     }else if (extraeDominio()!="gmail.com") {
+//         email.setCustomValidity("El dominio debe ser @gmail.com")
+//     }else{
+//         email.setCustomValidity("")
+//     }
+// }
+// email.addEventListener('blur',validaEmail)
+// validaEmail()
 
 
 //PASO 4: Eliminar validación automática del navegador
@@ -55,24 +53,73 @@ formulario.setAttribute('novalidate',true)
 
 //PASO 5: Añadir validación únicamente Javascript (Requisito paso 4)
 
-// function validaNombre() {
+function validaNombre() {
+    let b=false;
+    nombreError.classList.add("active")
+    if (!nombre.validity.valid){
+        nombreError.innerHTML="El nombre no puede estar vacío";
+    }else{
+        nombreError.innerHTML="";
+        nombreError.classList.remove("active")
+        b=true;
+    }
+    return b;
+}
+function validaEmail(){
+    let b=false;
+    emailError.classList.add("active")
+    if (email.validity.valueMissing){
+        email.setCustomValidity("El email es obligatorio");
+    }else if (extraeDominio()!="gmail.com") {
+        email.setCustomValidity("El dominio debe ser @gmail.com")
+    }else{
+        email.setCustomValidity("")
+        b=true;
+        emailError.classList.remove("active")
+    }
+    emailError.innerHTML=email.validationMessage;
+    return b;
+}
+/*
+function validaEdadHTML5(){
+    return true;
+}*/
 
-// }
-// function validaEmail(){
+function validaEdadJS(){
+    let b=false;
+    edadError.classList.add("active")
+    
+    if (edad.validity.valueMissing){
+        edad.setCustomValidity("La edad es obligatoria");
+    }else if (edad.validity.rangeOverflow) {
+        edad.setCustomValidity("La edad es demasiada cantidad");
+    }else if (edad.validity.rangeUnderflow) {
+        edad.setCustomValidity("Debe ser mayor de edad");
+    }else{
+        edad.setCustomValidity("");
+        b=true;
+        edadError.classList.remove("active")
+    }
+    edadError.innerHTML=edad.validationMessage;
+    return b;
+}
 
-// }
+function validaFormulario(event) {
+    let b=true;
+    if (!validaNombre()) {
+        b=false;
+    }
+    if (!validaEmail()) {
+        b=false;
+    }
+    if (!validaEdadJS()) {
+        b=false;
+    }
+    if (!b) {
+        event.preventDefault();    
+    }
+    return b;
+}
 
-// function validaEdadHTML5(){
-
-// }
-
-// function validaEdadJS(){
-
-// }
-
-// function validaFormulario(event) {
-
-// }
-
-
+formulario.addEventListener('submit',validaFormulario)
 
