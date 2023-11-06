38ebd79b94420bd242c46118bfc6b08e2a8fff81
Mattia Lu <mlu481@g.educaand.es>
2023-11-06 12:17:40 +0100

    1

---
+++ U2/actividad2/validacion.js
@@ -0,0 +1,54 @@
+function validaNombre() {
+    if (nombre.validity.valueMissing){
+        nombre.setCustomValidity("Campo requerido");
+    }else if(nombre.value.length>20){
+        nombre.setCustomValidity("Nombre demasiado largo");
+    }else if(nombre.validity.patternMismatch){  
+        nombre.setCustomValidity("Nombre invalido");
+    }else{
+        nombre.setCustomValidity("");
+    }
+}
+function validaEmail() {
+    if (email.value.indexOf("@")==0) {
+        email.setCustomValidity("Ese no es un email");
+    }else{
+        let dominio=email.value.substring(email.value.indexOf("@")+1)
+        if (email.validity.valueMissing){
+            email.setCustomValidity("Campo requerido");
+        }else if (email.validity.patternMismatch){
+            if (dominio!="vegasoft.com") {
+                email.setCustomValidity("Ese no es un email del dominio vegasoft.com");
+            }else{
+                email.setCustomValidity("No se aceptan caracteres especiales");
+            }
+        }else{
+            email.setCustomValidity("");
+        }
+    }
+}
+function validaTel() {
+    if (tel.validity.patternMismatch){
+        tel.setCustomValidity("telefono invalido");
+    }else{
+        tel.setCustomValidity("");
+    }
+}
+let patternComm=/^.{0,200}$/
+function validaComm() {
+    if (patternComm.test(comm.value)){
+        comm.setCustomValidity("");
+    }else{
+        comm.setCustomValidity("Maximo 200 caracteres");
+    }
+}
+const nombre=document.querySelector("#uname");
+const email=document.querySelector("#email");
+const tel=document.querySelector("#phone");
+const comm=document.querySelector("#comment");
+nombre.addEventListener('input',validaNombre);
+email.addEventListener('input',validaEmail);
+tel.addEventListener('input',validaTel)
+comm.addEventListener('input',validaComm)
+validaNombre()
+validaEmail()
