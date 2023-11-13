03b3c88e66dcfd7d422bb317c2ba72b54e566878
Mattia Lu <mlu481@g.educaand.es>
2023-11-13 11:13:09 +0100

    1

--- U3/actividad1/blackjack-js-Inicial/blackjack-js/assets/js/juego.js
+++ U3/actividad1/blackjack-js-Inicial/blackjack-js/assets/js/juego.js
@@ -5,8 +5,8 @@
  * 2S = Two of Spades
  */
 function barajaCompleta(){
+    //obtiene el mazo compleato
     mazo=[];
-    const link="assets/cartas/"
     let numero=["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
     let letra=["C","D","H","S"]
     for (let i = 0; i < numero.length; i++) {
@@ -19,38 +19,106 @@ function barajaCompleta(){
 function barajar(){
     mazo=_.shuffle(mazo);
 }
+//resetAll
 function newGame() {
     //rellenar el mazo
     barajaCompleta();
     //limpiar las dos zonas
-    zone=document.querySelector("#computadora-cartas")
-    zone.replaceChildren();
-    //dejar el jugador de carta por defecto
-    zone=document.querySelector("#jugador-cartas")
-    zone.replaceChildren();
-    pedir.classList.remove("invalid")
+    zonaPC.replaceChildren();
+    zonaUsuario.replaceChildren();
+    //resetear puntos
+    puntoUsuario.innerText=0;
+    puntoPC.innerText=0;
+    //reabilita los botones
+    pedir.disabled=false;
+    detener.disabled=false;
 }
 function sacar(){
+    //aunque nunca va a pasar
     if (mazo.length!=0) {
+        //obtiene la carta
         let carta=document.createElement("img");
         carta.classList.add("carta");
-        carta.src=mazo.pop();
-        zone.append(carta);
+        carta.src=mazo.shift();
+        zonaUsuario.append(carta);
+        //actualiza los puntos
+        let p=obtenerPunto(carta);
+        puntoUsuario.innerText=parseInt(puntoUsuario.innerText)+parseInt(p);
+        //boom
+        if (puntoUsuario.innerText>21) {
+            stop();
+        }
     }else{
         alert("Mazo vacío")
     }
 }
-function stop(){
-    pedir.classList.add("invalid")
+function obtenerPunto(carta) {
+    let c=carta.src
+    if (c.length>0) {
+        //obtiene el numero
+        let p=c.slice(c.lastIndexOf("/")+1,c.lastIndexOf(".")-1);
+        if (p<=10) {
+            p=p;
+        }else if(p=="A"){
+            p=11;
+        }else{
+            p=10;
+        }
+        return p;
+    }else{
+        alert("Error obtenerPunto")
+        return 0;
+    }
 }
+function stop(){
+    //desabilitar
+    pedir.disabled=true;
+    detener.disabled=true;
+    //turno de pc
+    sacarPC();
+}
+function sacarPC() {
+    //aunque nunca va a pasar
+    if (mazo.length!=0) {
+        //obtener la carta
+        let carta=document.createElement("img");
+        carta.classList.add("carta");
+        carta.src=mazo.shift();
+        zonaPC.append(carta);
+        //calcular los puntos
+        let p=obtenerPunto(carta);
+        puntoPC.innerText=parseInt(puntoPC.innerText)+parseInt(p);
+        //si el usuario ya ha hecho boom, hace solo una vez
+        if (puntoUsuario.innerText>21) {
+            return;
+        }
+        //si no es mayor que el usuario y pc sigue.(sin parseInt alguna vez da fallo, que no sigue)
+        if (parseInt(puntoPC.innerText)<=parseInt(puntoUsuario.innerText) && puntoPC.innerText<21) {
+            sacarPC();
+        }
+    }else{
+        alert("Mazo vacío")
+    }
+}
+//direccion de donde estan las imagenes
+const link="assets/cartas/"
+//el mazo
 var mazo=[];
+//botones
 let nuevo=document.querySelector("#btnNuevo");
 let pedir=document.querySelector("#btnPedir");
 let detener=document.querySelector("#btnDetener");
 
-//ese dirá donde se insertan las cartas.
-var zone=document.querySelector("#jugador-cartas");
+//donde se ponen las cartas
+var zonaUsuario=document.querySelector("#jugador-cartas");
+var zonaPC=document.querySelector("#computadora-cartas");
+
+//el elemento donde están los puntos
+var puntos=document.querySelectorAll("small")
+var puntoUsuario=puntos[0];
+var puntoPC=puntos[1];
 newGame();
+//eventListener
 nuevo.addEventListener('click',newGame);
 pedir.addEventListener('click',sacar);
 detener.addEventListener('click',stop);
