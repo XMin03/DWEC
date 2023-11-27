37eaf22863a65dc98cc25fa0103da950622a8d81
Mattia Lu <mlu481@g.educaand.es>
2023-11-27 11:58:08 +0100

    1

---
+++ U3/js-callbacks-promises/js-callbacks-promises/db.js
@@ -0,0 +1,64 @@
+const db = {
+    "peliculas": [
+      {
+        "id": 1,
+        "nombre": "El sexto sentido",
+        "director": "M. Night Shyamalan",
+        "clasificacionId": 1
+      },
+      {
+        "id": 2,
+        "nombre": "Pulp Fiction",
+        "director": "Tarantino",
+        "clasificacionId": 4
+      },
+      {
+        "id": 3,
+        "nombre": "Todo Sobre Mi Madre",
+        "director": "Almodobar",
+        "clasificacionId": 1
+      },
+      {
+        "id": 4,
+        "nombre": "300",
+        "director": "Zack Snyder",
+        "clasificacionId": 4
+      },
+      {
+        "id": 5,
+        "nombre": "El silencio de los corderos",
+        "director": "Jonathan Demme",
+        "clasificacionId": 1
+      },
+      {
+        "id": 6,
+        "nombre": "Forrest Gump",
+        "director": "Robert Zemeckis",
+        "clasificacionId": 2
+      },
+      {
+        "id": 7,
+        "nombre": "Las Hurdes",
+        "director": "Luis Buñuel",
+        "clasificacionId": 3
+      }
+    ],
+    "clasificaciones": [
+      {
+        "nombre": "Drama",
+        "id": 1
+      },
+      {
+        "nombre": "Comedia",
+        "id": 2
+      },
+      {
+        "nombre": "Documental",
+        "id": 3
+      },
+      {
+        "nombre": "Acción",
+        "id": 4
+      }
+    ]
+  }
