cb6307370e97313b21039b5b46bec0969fa98a0d
Mattia Lu <mlu481@g.educaand.es>
2023-11-28 11:57:43 +0100

    1

--- U3/Ejercicio3-tarea-js-promises/tarea-js-promises/main.js
+++ U3/Ejercicio3-tarea-js-promises/tarea-js-promises/main.js
@@ -1,1 +1,16 @@
-
+const url="http://localhost:3000/peliculas/"
+let enlaces=document.querySelectorAll("a");
+enlaces.forEach(a=>a.addEventListener("click",buscar));
+function buscar() {
+    console.log(this.innerText)
+}
+let listado=document.querySelector("ul");
+listado.innerHTML="";
+fetch(url).then((resp)=>resp.json().then(datas=>{datas.forEach(data=>{
+    let li=document.createElement("li")
+    let a=document.createElement("a")
+    a.innerHTML=data.nombre;
+    li.append(a);
+    listado.append(li);
+})
+})).catch(err=>console.log(err));
