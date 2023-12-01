8a260b03e8c1b0328c15f28dcc855ea2817d290b
Mattia Lu <mlu481@g.educaand.es>
2023-12-01 13:30:42 +0100

    1

--- U3/Ejercicio3-tarea-js-promises/tarea-js-promises/main.js
+++ U3/Ejercicio3-tarea-js-promises/tarea-js-promises/main.js
@@ -1,9 +1,9 @@
 const url="http://localhost:3000/peliculas/"
-let enlaces=document.querySelectorAll("a");
-enlaces.forEach(a=>a.addEventListener("click",buscar));
-function buscar() {
-    console.log(this.innerText)
-}
+let director=document.getElementById("director")
+let clasificacion=document.getElementById("clasificacion")
+let cartel=document.querySelector("#cartel img")
+let estrella=document.getElementsByClassName("estrellas")[0];
+
 let listado=document.querySelector("ul");
 listado.innerHTML="";
 fetch(url).then((resp)=>resp.json().then(datas=>{datas.forEach(data=>{
@@ -11,7 +11,22 @@ fetch(url).then((resp)=>resp.json().then(datas=>{datas.forEach(data=>{
     let a=document.createElement("a")
     a.innerHTML=data.nombre;
     a.href="#";
+    a.setAttribute("id",data.id);
     li.append(a);
+    a.addEventListener("click",buscar);
     listado.append(li);
 })
 })).catch(err=>console.log(err));
+
+function buscar() {
+    cartel.src="assets/imgs/loading.gif";
+    fetchSlow(url+this.id).then(resp=>resp.json()).then(data=>{
+        director.innerHTML=data.director;
+        clasificacion.innerHTML=data.clasificacion;
+        estrella.innerHTML="";
+        for (let index = 0; index < data.valoracion; index++) {
+            estrella.innerHTML+="<i class='fa fa-star'></i>";
+        }
+        cartel.src="assets/imgs/"+data.cartel;
+    })
+}
