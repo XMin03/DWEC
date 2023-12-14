750f86e139dcf4f988d3d1d4adb9b586ebbf6eb6
Mattia Lu <mlu481@g.educaand.es>
2023-12-14 13:14:18 +0100

    1

--- U3/examenDWEC depuración/Proyecto5/main.js
+++ U3/examenDWEC depuración/Proyecto5/main.js
@@ -1,13 +1,8 @@
 //Al pulsar el botón, la casilla3 cambia el texto de "Sin pulsar" a "Botón pulsado"
-
 const boton = document.getElementById('pulsame');
-function mostrarAviso() {
-    casilla3.innerText = 'Botón pulsado';
-}
-boton.addEventListener('click', mostrarAviso);
-/*
+
 function mostrarAviso(texto) {
     casilla3.innerText = texto;
-}*/
-
+}
+boton.addEventListener('click', ()=>mostrarAviso('Botón pulsado'));
 //boton.addEventListener('click', mostrarAviso('Botón pulsado'));
