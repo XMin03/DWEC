1a44822a78a46e28a68ee08f2bdaf7d4f6cdba3c
Mattia Lu <mlu481@g.educaand.es>
2024-01-12 14:08:34 +0100

    1

---
+++ dev/angular/ejercicio1/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Mattia';
+  mostrar=true;
+  lista=["〇","一","二","三","亖"];
+}
