cb2cbab57e2654641691cd57e34812b41f6e0e4a
Mattia Lu <mlu481@g.educaand.es>
2024-01-15 11:02:26 +0100

    1

--- dev/angular/ejercicio1/src/app/app.component.ts
+++ dev/angular/ejercicio1/src/app/app.component.ts
@@ -2,18 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, BodyComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Mattia';
-  mostrar=true;
-  lista=["零","壹","貳","叁","肆","伍","陸","柒","捌","玖","拾"];
-  cambiar(){
-    this.mostrar=!this.mostrar;
-  }
+ 
 }
--- dev/angular/ejercicio1/src/app/components/body/body.component.ts
+++ dev/angular/ejercicio1/src/app/components/body/body.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
-
+  mostrar : boolean = true;
+  lista:String[] = ["零", "壹", "貳", "叁", "肆", "伍", "陸", "柒", "捌", "玖", "拾"];
+  cambiar() {
+    this.mostrar = !this.mostrar;
+  }
 }
