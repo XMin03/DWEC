0f77c0e1dc2416a798c6be1fea8dae8bcbdee561
Mattia Lu <mlu481@g.educaand.es>
2024-01-15 12:20:10 +0100

    1

---
+++ dev/angular/ejercicio1/src/app/contador/contador.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  standalone: true,
+  imports: [],
+  templateUrl: './contador.component.html',
+  styleUrl: './contador.component.css'
+})
+export class ContadorComponent {
+  cont:number=0;
+  click(){
+    this.cont++;
+  }
+}
