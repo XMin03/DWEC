359f4dad1b647cf3eb8e4a96247547b86b8a1763
Mattia Lu <mlu481@g.educaand.es>
2024-01-16 09:56:37 +0100

    1

--- dev/angular/actividad2/src/app/productos/listado/listado.component.ts
+++ dev/angular/actividad2/src/app/productos/listado/listado.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-listado',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './listado.component.html',
   styleUrl: './listado.component.css'
 })
 export class ListadoComponent {
-
+  productos:any[]=[
+    {nombre:"Mesa",precio:112.5,categoria:"Mobiliario"},
+    {nombre:"Taladro",precio:22.5,categoria:"Herramientas"},
+    {nombre:"Soplete",precio:52.75},
+    {nombre:"Silla",precio:42.5,categoria:"Mobiliario"}
+  ]
+  eliminados:any[]=[]
+  eliminar(index:number){
+    this.eliminados.push(this.productos.splice(index,1)[0]);
+  }
 }
