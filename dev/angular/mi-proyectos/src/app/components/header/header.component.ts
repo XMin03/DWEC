4a279678b99d9693eca01705be6b2ec04d72c3dd
Mattia Lu <mlu481@g.educaand.es>
2024-02-06 10:30:04 +0100

    1

---
+++ dev/angular/mi-proyectos/src/app/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"app-header",
+    template: `<h1>CABECERA</h1>`,
+    standalone:true,
+})
+export class HeaderComponent {
+}
