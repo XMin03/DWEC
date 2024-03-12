3181511e0d0293258c7ab309a8434bd9142114be
Mattia Lu <mlu481@g.educaand.es>
2024-03-12 12:17:12 +0100

    1

--- dev/angular/examen-marzo/src/app/components/padre/padre.component.ts
+++ dev/angular/examen-marzo/src/app/components/padre/padre.component.ts
@@ -2,45 +2,43 @@ import { Component, EventEmitter } from '@angular/core';
 import { HijoComponent } from '../hijo/hijo.component';
 import { NgFor, NgIf } from '@angular/common';
 
-const DIAS:string[]=['lunes', 'martes', 'miércoles', 'jueves', 'viernes'];
-const ESTACIONES:string[] = ['primavera', 'verano', 'otoño', 'invierno'];
+const DIAS: string[] = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes'];
+const ESTACIONES: string[] = ['primavera', 'verano', 'otoño', 'invierno'];
 
 @Component({
   selector: 'app-padre',
   standalone: true,
-  imports: [HijoComponent,NgFor,NgIf],
+  imports: [HijoComponent, NgFor, NgIf],
   templateUrl: './padre.component.html',
   styleUrl: './padre.component.css'
 })
 export class PadreComponent {
-  habilitar:EventEmitter<any>= new EventEmitter();
+  habilitar: EventEmitter<any> = new EventEmitter();
 
-  seleccionado_dias:boolean=true;
-  botones_por_pulsar:string[] = [];
-  botones_pulsados:string[] = [];
-  ngOnInit(){
+  seleccionado_dias: boolean = true;
+  botones_por_pulsar: string[] = [];
+  botones_pulsados: string[] = [];
+  ngOnInit() {
     this.useDias();
   }
-  useDias(){
-    if (this.botones_por_pulsar!=DIAS) {
-      this.botones_por_pulsar=DIAS
-      this.botones_pulsados= [];  
-    }
+  useDias() {
+    this.botones_por_pulsar = DIAS
+    this.botones_pulsados = [];
+    this.seleccionado_dias = true
   }
-  useEstaciones(){
-    if (this.botones_por_pulsar!=ESTACIONES) {
-      this.botones_por_pulsar=ESTACIONES
-      this.botones_pulsados= [];
-    }
+  useEstaciones() {
+    this.botones_por_pulsar = ESTACIONES
+    this.botones_pulsados = [];
+    this.seleccionado_dias = false
   }
-  constructor(){
+  constructor() {
 
   }
-  reset(){
-    this.botones_pulsados= [];
+  reset() {
+    this.botones_pulsados = [];
     this.habilitar.emit();
   }
-  suma(indice:number){
-    this.botones_pulsados.push(this.botones_por_pulsar.at(indice)as string)
+  suma(indice: number) {
+    this.botones_pulsados.push(this.botones_por_pulsar.at(indice) as string)
   }
 }
